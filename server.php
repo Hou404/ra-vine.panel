35671c5b6e7f539f4c2a4ab6df9a1e580e3272a5
ItsmeHanz <130408892+Hou404@users.noreply.github.com>
2023-04-15 04:52:34 +0700

    Update server.php

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   Clavertz <clave@laravel.com>
  */
 
 $uri = urldecode(
